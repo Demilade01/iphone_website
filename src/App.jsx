83e752f9a3798f57da14382dadb5527ac48abe97
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-05-08 14:04:09 -0700

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Hero, Highlights, Navbar } from "./components"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Highlights from './Highlights'
+
+export {
+  Navbar, Hero, Highlights
+}
