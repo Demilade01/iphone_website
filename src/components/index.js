866793cbd8f3399dfe7cade376b04e1be9287428
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-05-09 19:08:10 -0700

    videocarousel

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Highlights from './Highlights'
+import VideoCarousel from './VideoCarousel'
 
 export {
-  Navbar, Hero, Highlights
+  Navbar, Hero, Highlights, VideoCarousel
 }
