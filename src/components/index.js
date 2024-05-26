6206383a327462043dc637e2d7b4dc5556a92dd5
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-05-26 14:07:43 -0700

    implement models

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Highlights from './Highlights'
+import Model from './Model'
 import VideoCarousel from './VideoCarousel'
+import ModelView from './ModelView'
+import Lights from './Lights'
 
 export {
-  Navbar, Hero, Highlights, VideoCarousel
+  Navbar, Hero, Highlights, VideoCarousel, Model, ModelView, Lights
 }
